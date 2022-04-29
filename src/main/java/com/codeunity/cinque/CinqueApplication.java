040f735fe1927d91cfab9535feb4ff3e235a3e15
hasindusithmin <hasindusithmin64@gmail.com>
2022-04-29 19:43:40 +0530

    Create spring boot project

---
+++ src/main/java/com/codeunity/cinque/CinqueApplication.java
@@ -0,0 +1,13 @@
+package com.codeunity.cinque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeunity/cinque/CinqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeunity.cinque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
